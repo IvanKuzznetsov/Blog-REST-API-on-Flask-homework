92138386697e02aee61995a83b9f4bcab53badb5
IvanKuzznetsov <ivan.kuzznetsov@gmail.com>
2024-02-20 16:38:39 +0700

    Add function: read_twits Add function: to_dict

--- model/twit.py
+++ model/twit.py
@@ -6,3 +6,6 @@ class Twit:
     def __init__(self, body: str, author: User):
         self.body = body
         self.author = author
+
+    def to_dict(self):
+        return {"body": self.body, "author": self.author}
